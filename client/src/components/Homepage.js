0d9213f0ded1e781ca727927413fb6bd7813254e
Avijit Choudhury <avijit.choudhury24@gmail.com>
2019-11-22 18:03:12 -0500

    added react routes for basic skeleton

---
+++ client/src/components/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Homepage () {
+  return (
+    <div>
+      I am home page <br/>
+      <button>Add Entry</button>
+      <button>Add Category</button>
+
+    </div>
+  )
+}
