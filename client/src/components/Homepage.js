dbb43afe93127ef5e0fc0c472644b7d13a9e1637
Aiman Azmie <aimanaaw@gmail.com>
2019-11-23 14:48:07 -0500

    Made changes to connect the react routes to the nodejs

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 export default function Homepage () {
+
+  const [state, setState] = useState()
+
+
+  useEffect(() => {
+    Promise.all([
+      axios.get('http://localhost:3001/categories')
+    ])
+    .then(function(values) {
+      console.log(values)
+      setState(...values)
+    })
+  }, [])
   return (
     <div>
+      {state}
       I am home page <br/>
       <button>Add Entry</button>
       <button>Add Category</button>
