161e48c8464f697ebcf88fe5ada4ed89c2be404b
Aiman Azmie <aimanaaw@gmail.com>
2019-11-22 19:31:40 -0500

    Added the queries

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+const db = require('./queries')
 
 app.use(bodyParser.json())
 app.use(
--- server/queries.js
+++ server/queries.js
@@ -6,3 +6,14 @@ const pool = new Pool({
   password: 'expense',
   port: 5432,
 })
+
+const getCategories = async (req, res) => {
+  await pool.query(`SELECT * FROM categories`, (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(results.rows)
+  })
+}
+
+module.exports = {getCategories}
