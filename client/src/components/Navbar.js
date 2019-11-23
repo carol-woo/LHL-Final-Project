034c3f50e5f44325a1deaf1b7b76e637ff70d1e3
Araxiah <cewoo98@hotmail.com>
2019-11-23 16:23:03 -0500

    nav bar is now moved to the side

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -16,11 +16,11 @@ export default function Navbar(){
   return(
       <div className='nav'>
         <BrowserRouter>
-        I am the NavBar!
         <nav>
-          <button><Link to="/">Homepage</Link></button>
-          <button><Link to="/new-entry"> New Entry </Link></button>
-          <button><Link to="/monthly-view">Monthly View</Link></button>
+        I am the NavBar! <br/>
+          <button><Link to="/">Homepage</Link></button> <br/>
+          <button><Link to="/new-entry"> New Entry </Link></button> <br/>
+          <button><Link to="/monthly-view">Monthly View</Link></button> <br/>
           <button>Logout</button>
         </nav>
 
