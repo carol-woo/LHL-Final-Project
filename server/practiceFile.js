faaa6bfeb49a30f6de6632d294372c349a065d89
Aiman Azmie <aimanaaw@gmail.com>
2019-11-22 19:02:01 -0500

    Setup the express server

---
+++ server/practiceFile.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (req, res) => {
+  res.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+
+
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
