0bb88cdaab067e13dabc952a5f1dc730b61ec857
Araxiah <cewoo98@hotmail.com>
2019-11-25 15:22:13 -0500

    basic login input setup

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,17 +1,29 @@
-import React from "react"
+import React, {useState} from "react"
 
 
 
 //For Login view
 export default function Login(){
-  
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+
   return(
     <div>
       I am temp login!
       <form>
+
         Email
           <input
+            type ="text"
             email={email}
+            onChange={setEmail}
+          />
+
+        Password
+          <input
+            type="password"
+            password={password}
+            onChange={setPassword}
           />
       </form>
     </div>
