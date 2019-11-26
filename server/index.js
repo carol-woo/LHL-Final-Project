fa821154ef11905bee085bb56834773ee0950f65
Araxiah <cewoo98@hotmail.com>
2019-11-26 14:37:02 -0500

    logout route added, still need to terminate cookie but post hits the logout route

--- server/index.js
+++ server/index.js
@@ -5,6 +5,13 @@ const port = 3001
 const db1 = require('./queries_tier_1')
 const db2 = require('./queries_tier_2')
 var cors = require('cors')
+
+const cookieSession = require('cookie-session');
+app.use(cookieSession({
+  name: 'session',
+  keys: ["POTATO"],
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
  
 app.use(cors())
 
@@ -18,6 +25,31 @@ app.use(
 // app.get('/', (req, res) => {
 //   res.json({ info: 'Node.js, Express, and Postgres API' })
 // })
+
+//login
+app.post('/login', async (req, res) => {
+  const user = await db2.userVerification(req.body.email, req.body.password)
+  req.session.user_id = user.id
+  console.log('/LOGIN HIT')
+  res.json({status: 'ok'})
+})
+
+app.get('/logout',  (req, res) => {
+//  if(req.session.user && req.session.user_id){
+//    res.clearCookie('user_id')
+//  }
+//   console.log("Nuke my Cookie index.js!")
+//   console.log("what is my session?", req.session)
+
+// req.session.destroy((err) => {
+//   if(err){
+//     console.log(err)
+//   }
+// })
+
+req.session = null
+})
+
 app.post('/new-user', db1.addUser)
 app.get('/categories', db1.getCategories)
 app.post('/new-category/:id', db1.addCategory)
@@ -26,9 +58,6 @@ app.get('/transactions', db1.getTransactions)
 app.post('/new-entry', db1.addTransaction)
 app.put('/transactions/:id', db1.editTransaction)
 app.delete('/transactions:id', db1.deleteTransaction)
-
-//login
-app.post('/login', db2.userVerification)
 
 
 
