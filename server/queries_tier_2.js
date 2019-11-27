dc890d1bbe206bb9347e48b8d3c932a048a2f7e2
Aiman <aimanaaw@gmail.com>
2019-11-27 10:14:49 -0500

    Changes to queries

--- server/queries_tier_2.js
+++ server/queries_tier_2.js
@@ -15,26 +15,30 @@ const userVerification = async (email, password) => {
   return res.rows[0]
 }
 
-const getHouseholdsCategories = async (req, res) => {
-  await pool.query(`SELECT * FROM categories join households on households.id = categories.household_id where household_id='1'`, (error, results) => {
-    if (error) {
-      throw error
-    }
-    console.log("getHouseholdCategories being used in queries_tier2.js", results.rows)
-    res.status(200).json(results.rows)
-  })
-}
+// const getHouseholdsCategories = async (req, res) => {
+//   await pool.query(`SELECT * FROM categories join households on households.id = categories.household_id where household_id='1'`, (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     console.log("getHouseholdCategories being used in queries_tier2.js", results.rows)
+//     res.status(200).json(results.rows)
+//   })
+// }
 
-// const getHouseholdsCategories = async () => {
-//   console.log("TEST")
-//   const result =  await pool.query(`
-//      SELECT * FROM categories;`);
-//     //   M categories
-//     //  join households on households.id = categories.household_id
-//     //   where household_id='1';`);
-//   //  return res.json(res.rows);
-//   console.log(result.rows)
-//   return JSON.stringify(result.rows[0]);
-//  }
+const getHouseholdsCategories = async () => {
+  console.log("TEST")
+  try {
+    const result =  await pool.query(`
+       SELECT * FROM categories`);
+      //   M categories
+      //  join households on households.id = categories.household_id
+      //   where household_id='1';`);
+    //  return res.json(res.rows);
+    console.log("getHouseholdCategories being used in queries_tier2.js", result.rows[0])
+    return result.rows;
+  } catch (error) {
+    console.log(error)
+  }
+ }
 
 module.exports = {userVerification, getHouseholdsCategories}
