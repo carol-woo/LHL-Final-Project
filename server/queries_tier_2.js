291e2ef32a0fb0fc40197cd6a721fe35332c2607
Aiman <aimanaaw@gmail.com>
2019-11-27 16:18:07 -0500

    Query modifications

--- server/queries_tier_2.js
+++ server/queries_tier_2.js
@@ -15,17 +15,6 @@ const userVerification = async (email, password) => {
   return res.rows[0]
 }
 
-// const getHouseholdsCategories = async (req, res) => {
-//   const householdId = req.params
-//   await pool.query(`SELECT * FROM categories join households on households.id = categories.household_id where household_id='1'`, (error, results) => {
-//     if (error) {
-//       throw error
-//     }
-//     console.log("getHouseholdCategories being used in queries_tier2.js", results.rows)
-//     res.status(200).json(results.rows)
-//   })
-// }
-
 getUsercategories = async(user_id) => {
   try {
    let aa = await pool.query(`SELECT categories.* FROM categories join users on categories.user_id = users.id where categories.user_id='1'`);
