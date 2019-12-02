f37949f04cb3c7f0bc444faa8ba647e6d4b0659d
Araxiah <cewoo98@hotmail.com>
2019-12-02 17:17:23 -0500

    added aria labels to the buttons on delete

--- client/src/components/Delete.js
+++ client/src/components/Delete.js
@@ -17,9 +17,9 @@ export default function Delete(props){
 
 return (
   <div>
-    <button className="yes"onClick={deleteTransaction}></button>
+    <button className="yes"onClick={deleteTransaction} aria-label="Green check mark for deleting transaction"></button>
     Are you sure you want to delete {props.store_name}?
-    <button className="no"onClick={props.renderDelete}></button>
+    <button className="no"onClick={props.renderDelete} aria-label="No button for not wanting to delete a transaction"></button>
   </div>
 )
 }
