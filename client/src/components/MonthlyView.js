3046dd15389e280aee76733143145a2e26888976
Avijit Choudhury <avijit.choudhury24@gmail.com>
2019-12-02 14:48:20 -0500

    income changes

--- client/src/components/MonthlyView.js
+++ client/src/components/MonthlyView.js
@@ -33,7 +33,7 @@ useEffect(() => {
     margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
     <CartesianGrid strokeDasharray="3 3" />
     <XAxis dataKey="name" />
-    <YAxis />
+    <YAxis type="number"/>
     <Tooltip />
     <Legend />
     <Line type="monotone" dataKey="Average daily budget" stroke="#8884d8" />
