57d8aa4e41909f0243d13d74188d22bbc479ae03
Aiman <aimanaaw@gmail.com>
2019-12-01 21:27:28 -0500

    Fix for login

--- server/queries_tier_3.js
+++ server/queries_tier_3.js
@@ -8,13 +8,6 @@ const pool = new Pool({
 })
 
 
-const userVerification = async (email,  password) => {
-  const res =  await pool.query(`
-  SELECT * FROM users
-  WHERE email=$1 AND password_digest=$2;`,[email,password]);
-  return res.rows[0]
-}
-
 const getMonthAverageBudget = async (month, year) => {
   const monthAverageBudget = await pool.query(`select avg(transactions.amount) as average from transactions
   where extract (month from entered_on) = $1 and extract (year from entered_on) = $2`, [month, year])
@@ -22,48 +15,19 @@ const getMonthAverageBudget = async (month, year) => {
   return monthAverageBudget;
 }
 
-getUsercategories = async(user_id) => {
-  try {
-    let aa = await pool.query(`
-    SELECT categories.id, categories.name, categories.category_budget,  SUM(transactions.amount)
-    FROM categories  
-    JOIN users ON categories.user_id = users.id 
-    LEFT JOIN transactions ON categories.id = transactions.category_id
-    WHERE categories.user_id=${user_id}
-    GROUP BY categories.id, categories.name, categories.category_budget;
-    `);
-    console.log("I AM ROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOWS",aa.rows);
-    return aa;
-  } catch (error) {
-    console.error(error);
-  }
+const amountSpentPerDayMonth = async (month, user_id) => {
+  const amountSpentPerDay = await pool.query(`select sum(amount), entered_on from transactions where extract (month from entered_on) = $1 and user_id = $2 group by entered_on`, [month, user_id])
+  console.log("Testing the amountSpentPerDayMonth in queries_tier_3", amountSpentPerDay);
+  return amountSpentPerDay;
 }
 
-// getCategoriesAmount = async() => {
-  //   try {
-    //    let aa = await pool.query(`select sum(transactions.amount), category_id from transactions group by category_id;`);
-    //    console.log(aa.rows);
-    //    return aa;
-    //   } catch (error) {
-      //     console.error(error);
-      //   }
-      // }
+// const amountBudg
+
+// select sum(transactions.amount), category_id, categories.name, categories.category_budget, transactions.user_id from transactions join categories on categories.id = transactions.category_id group by transactions.category_id, categories.name, categories.category_budget, transactions.user_id having transactions.user_id = 1
+
+
       
-      getCategoryTransactions = async(category_id) => {
-        try {
-          let transactions = await pool.query(`
-          select *, transactions.id as transaction_id from transactions 
-          join categories on categories.id = category_id 
-          where category_id = ${category_id};`);
-          console.log("TESTING TRA", transactions)
-          return transactions;
-          // return await pool.query(`SELECT * FROM categories`);
-        } catch (error) {
-          console.error(error);
-        }
-      }
-      
-      module.exports = {getMonthAverageBudget}
+      module.exports = {getMonthAverageBudget, amountSpentPerDayMonth}
       
       // select budget, expenditure, (budget - expenditure) as balance from (select category_budget as budget, transactions.amount as expenditure
       //   from categories
