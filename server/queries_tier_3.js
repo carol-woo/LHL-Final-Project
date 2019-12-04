0e5ff3969835508e2033e05e2b8fb8bcdfcfb188
Aiman <aimanaaw@gmail.com>
2019-12-04 17:12:58 -0500

    Altering the query

--- server/queries_tier_3.js
+++ server/queries_tier_3.js
@@ -11,8 +11,12 @@ const pool = new Pool({
 amountSpentPerDayMonth = async (user_id) => {
   const result = await pool.query(`select sum(amount) as total, extract (day from entered_on) as day from transactions where extract (month from entered_on) = 6 and user_id = $1 group by entered_on`, [user_id]);
 
-  const monthAverageBudget = await pool.query(`select avg(transactions.amount) as dailyAverage from transactions
-  where extract (month from entered_on) = 6 and extract (year from entered_on) = 2019 and user_id = $1`, [user_id]);
+  // const monthAverageBudget = await pool.query(`select avg(transactions.amount) as dailyAverage from transactions
+  // where extract (month from entered_on) = 6 and extract (year from entered_on) = 2019 and user_id = $1`, [user_id]);
+  const usersBudget = await pool.query(`select users.budget from users where users.id = $1`, [user_id]);
+  console.log("TESTING", usersBudget.rows[0].budget)
+  const monthAverageBudget = (usersBudget.rows[0].budget) / 30;
+  console.log("TESTING MONTH", monthAverageBudget)
   // console.log ("Testing the avg query", monthAverageBudget);
 
   const categoryAmountSpentMonth = await pool.query('select categories.name, categories.category_budget, sum(transactions.amount) as total from transactions join categories on categories.id = transactions.category_id where extract(month from entered_on) = 6 and transactions.user_id = $1 group by categories.name, categories.category_budget', [user_id]);
@@ -20,7 +24,7 @@ amountSpentPerDayMonth = async (user_id) => {
 
   let payload = {
     dailyTotalTransactions: result.rows,
-    average: monthAverageBudget.rows[0].dailyaverage,
+    average: monthAverageBudget,
     totalCategorySpentMonth: categoryAmountSpentMonth.rows
   };
     return payload;
